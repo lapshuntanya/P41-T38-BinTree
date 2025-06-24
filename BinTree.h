a9b5394eea0a60b77021207b04e59696e20903bc
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-24 17:02:25 +0300

    https://github.com/lapshuntanya/P41-T38-BinTree.git

---
+++ BinTree.h
@@ -0,0 +1,76 @@
+//
+// Created by Lapshun Tetiana on 24.06.2025.
+//
+
+#ifndef P41_T38_BINTREE_BINTREE_H
+#define P41_T38_BINTREE_BINTREE_H
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Node {
+public:
+    T info;
+    Node* left;
+    Node* right;
+
+    Node(T value) { info = value; left = right = nullptr; }
+    void showNode() {
+        cout << "INFO: " << info << endl;
+        cout << "\t Left: " << left << " Right: " << right << endl;
+    }
+};
+
+
+template <typename U>
+class BinTree {
+    Node<U>* root; //Вказівник на перший вузол у дереві
+public:
+    BinTree() { root = nullptr; }
+    ~BinTree() {}
+
+    void insert(U value) {
+        Node<U>* el = new Node<U>(value);
+
+        if(root == nullptr)
+            root = el;
+        else{
+            //Шукаємо місце для нового вузла
+            Node<U> * p = root;
+            Node<U> * pPrev = root;
+            while(p!= nullptr)
+            {
+                pPrev = p;
+               // p = value < p->info ? p->left : p->right;
+               if( value < p->info)
+                   p = p->left;
+               else
+                   p = p->right;
+            }
+
+            if(value < pPrev->info)
+                pPrev->left = el;
+            else
+                pPrev->right = el;
+        }
+    }
+
+    Node<U>* search(U value) {
+        //Повернути адресу вузла зі значенням value
+        //Якщо value немає в дереві, то повернути nullptr
+    }
+
+    //Повний обхід дерева
+    void showTree() {
+        if (root == nullptr)
+            cout << "Дерево контейнерів порожнє!" << endl;
+        else {
+            // . . .
+        }
+    }
+
+};
+
+
+
+#endif //P41_T38_BINTREE_BINTREE_H
